b0e2eb35a417e113e6b8a5b2f512102dc562355a
PKullberg <peter.kullberg@helsinki.fi>
2018-03-26 11:15:07 +0300

    Small fizes here and there

--- src/create_spp_files.R
+++ src/create_spp_files.R
@@ -25,13 +25,13 @@ birds <- read.table("zruns/2016_data/general_files/new_birds.spp")
 mammals <- read.table("zruns/2016_data/general_files/new_mammals.spp")
 
 all_spp <- rbind(amphibians, birds, mammals)
+# all_spp <- read.table("zruns/2016_data/general_files/all_vertebrate.spp")
 
 # make paths spp files relative to the project folder 
 terrestrial_threatened_spp <- all_spp %>% mutate(V6 = gsub("/wrk/pkullber/zonation_runs/KBA_priority/" ,"../../../" ,V6))
 
 # Filter out purely marine spp
 ## this is a bit tricky way to do this but IUCN RLI portal does not allow "select not ..." operations, or provide info on the system in the data table.
-
 ### terrestrial tetrapods from IUCN RL, downloaded 12 / 2016, search by system: marine, include sub species
 marines <- read.csv("data/spp_info/marin_IUCN_search_2016.csv")
 ### terrestrial tetrapods from IUCN RL, downloaded 12 / 2016, search by system: marine + terrestrial, include sub species
@@ -63,13 +63,13 @@ terrestrial_DD0_spp <- terrestrial_threatened_and_dd_spp %>%
 
 
 # make spp fille with all data
-terrestrial_threatened_dd_and_gbif_spp_status <- rbind(terrestrial_threatened_and_dd_spp, GBIF_spp) # leave status for later use
+terrestrial_threatened_dd_and_gbif_spp_status <- bind_rows(terrestrial_threatened_and_dd_spp, GBIF_spp) # leave status for later use
 terrestrial_threatened_dd_and_gbif_spp <- select(terrestrial_threatened_dd_and_gbif_spp_status, -Status, -key)
 
 # make spp fille where gbif and DD species have 0 weight (They don't not affect the prioritization, but their status can be followed)
 GBIF_spp <- data.frame(V1 = 0, V2 = 1, V3 = 1, V4 = 1, V5 = 0.25, V6 = GBIF_files)
-terrestrial_threatened_dd_and_gbif0_spp <- bind(terrestrial_threatened_and_dd_spp, GBIF_spp)
-terrestrial_threatened_dd0_and_gbif0_spp <- bind(terrestrial_DD0_spp, GBIF_spp)
+terrestrial_threatened_dd_and_gbif0_spp <- bind_rows(terrestrial_threatened_and_dd_spp, GBIF_spp)
+terrestrial_threatened_dd0_and_gbif0_spp <- bind_rows(terrestrial_DD0_spp, GBIF_spp)
 
 write.table(terrestrial_threatened_dd_and_gbif_spp, "zruns/2016_data/general_files/threatened_only_TW_relative_no_marines.spp", row.names = F, col.names = F)
 write.table(terrestrial_threatened_dd_and_gbif0_spp, "zruns/2016_data/general_files/threatened_and_GBIF05_TW_relative_no_marines.spp", row.names = F, col.names = F)
